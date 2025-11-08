40ebfcfed781cd2838249b3f4c666e0deb4499b5
ytakahashi2020 <y.takahashi2020@gmail.com>
2025-11-09 08:14:43 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import {
+  Keypair,
+  PublicKey,
+  VersionedTransaction,
+  Connection,
+} from "@solana/web3.js";
+
+import bs58 from "bs58";
+import "dotenv/config";
+
+const secretKeyBase58 = process.env.SECRET_KEY!;
+const secretKey = bs58.decode(secretKeyBase58);
+const keypair = Keypair.fromSecretKey(secretKey);
+
+const network = "mainnet-beta";
+const connection = new Connection("https://api.mainnet-beta.solana.com");
+const wallet = {
+  network,
+  publicKey: keypair.publicKey,
+  updateTransaction: async (tx: VersionedTransaction) => {
+    tx.sign([keypair]);
+    return tx;
+  },
+};
+
+const response1 = await fetch("https://api.mainnet-beta.solana.com", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    jsonrpc: "2.0",
+    id: 1,
+    method: "getHealth",
+  }),
+});
+
+const data1 = await response1.json();
+console.log(data1);
+
+const response2 = await fetch("https://helius.api.corbits.dev", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    jsonrpc: "2.0",
+    id: 1,
+    method: "getBlockHeight",
+  }),
+});
+
+const data2 = await response2.json();
+console.log(data2);
